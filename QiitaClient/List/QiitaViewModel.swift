6688da16f0762aa8dfb4147c2b9bbc5ef65495a6
miserii <miseri.osk12@gmail.com>
2021-01-16 12:45:48 +0900

    create qiitaViewModel

---
+++ QiitaClient/List/QiitaViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  QiitaViewModel.swift
+//  QiitaClient
+//
+//  Created by osakamiseri on 2021/01/16.
+//
+
+import Foundation
+import RxSwift
+
+final class QiitaViewModel {
+
+    private let qiitaModel = QiitaModel()
+
+}
