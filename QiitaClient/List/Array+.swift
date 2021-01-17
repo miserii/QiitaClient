223fe3a102e9f8eb8741d163b46de1106b76a062
miserii <miseri.osk12@gmail.com>
2021-01-18 00:50:24 +0900

    fix array

--- QiitaClient/List/Array+.swift
+++ QiitaClient/List/Array+.swift
@@ -6,8 +6,8 @@
 //
 
 import Foundation
-import UIKit
 
+//安全に使えるようにしてるクラッシュを避けのお守り
 extension Array {
   subscript (safe index: Int) -> Element? {
     return index >= 0 && index < self.count ? self[index] : nil
